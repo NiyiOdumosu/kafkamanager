37edf403660e6c542c1a65868ca5babbeea1a542
VEN-Odumosu, Niyi <ven-odumosun@jncb.com>
2023-11-22 15:35:22 -0500

    Added the deepdive logic and changed the json structure

--- generatetopics.py
+++ generatetopics.py
@@ -1,35 +1,38 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('topic_configs.csv')
+df = pd.read_csv('application1/topics/topic_configs.csv')
 
 topics_list = []
 
 # Add document link to the future topic configs
 
 for index, row in df.iterrows():
+    topic_name = row['topic name']
     topic_dict = {
-        "topic_name": row['topic name'],
-        "partitions_count": str(row['partition count']),
-        "replication_factor": 3,
-        "configs": [
-            {
-                "name": "cleanup.policy",
-                "value": str(row['cleanup.policy'])
-            },
-            {
-                "name": "compression.type",
-                "value": str(row['compression.type'])
-            },
-            {
-                "name": "retention.ms",
-                "value": str(row['retention.ms'])
-            },
-            {
-                "name": "segment.bytes",
-                "value": str(row['segment.bytes'])
-            }
-        ]
+        f"{topic_name}" : {
+            "topic_name": row['topic name'],
+            "partitions_count": str(row['partition count']),
+            "replication_factor": 3,
+            "configs": [
+                {
+                    "name": "cleanup.policy",
+                    "value": str(row['cleanup.policy'])
+                },
+                {
+                    "name": "compression.type",
+                    "value": str(row['compression.type'])
+                },
+                {
+                    "name": "retention.ms",
+                    "value": int(row['retention.ms'])
+                },
+                {
+                    "name": "segment.bytes",
+                    "value": int(row['segment.bytes'])
+                }
+            ]
+        }
     }
     topics_list.append(topic_dict)
 
--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,12 @@
 from github import Github
 import logging
 import os
+import json
+from deepdiff import DeepDiff
 
 # Constant variables
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
-#set up logger
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,13 +17,52 @@ def get_latest_commit_diff():
         g = Github(GITHUB_TOKEN)
         repo = g.get_repo("NiyiOdumosu/kafkamanager")
         latest_commit = repo.get_commits()[0]
-        diff = latest_commit.files[0]
-        print(diff)
-        return diff
+        feature_topic_content = repo.get_contents("application1/topics/topics.json", ref="test")
+        main_topic_content = repo.get_contents("application1/topics/topics.json", ref="main")
+        old_topics = json.loads(feature_topic_content.decoded_content)
+        new_topics = json.loads(main_topic_content.decoded_content)
+
+        return old_topics, new_topics
     except Exception as e:
         logger.error(f"Error getting latest commit diff: {e}")
         raise
 
 
+def load_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def find_changed_topics(old_topics, new_topics):
+    old_topics_dict = {}
+    new_topics_dict = {}
+
+    for value in old_topics:
+        old_topics_dict.update(value)
+
+    for value in new_topics:
+        new_topics_dict.update(value)
+
+    changed_topic_names = []
+    # Check for changes and deletions
+    for topic_name, old_topic in old_topics_dict.items():
+        new_topic = new_topics_dict.get(topic_name)
+        if new_topic:
+            diff = DeepDiff(old_topic, new_topic, ignore_order=True)
+            if diff:
+                changed_topic_names.append({topic_name: diff, "type": "update"})
+        else:
+            # Topic was removed
+            changed_topic_names.append({topic_name: diff, "type": "removed"})
+
+    # Check for new additions
+    for topic_name in new_topics_dict:
+        if topic_name not in old_topics_dict.keys():
+            changed_topic_names.append({topic_name: new_topics_dict.get(topic_name), "type": "new"})
+
+    return changed_topic_names
+
+
 if __name__ == "__main__":
-    diff = get_latest_commit_diff()
+    old_topics, new_topics = get_latest_commit_diff()
+    print(find_changed_topics(old_topics, new_topics))
